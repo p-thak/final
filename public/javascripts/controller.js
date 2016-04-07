e712a4a0de59e614179e1f4c3fb7f3154f0af01c
jjaywhitaker <jjaywhitaker@gmail.com>
2016-04-07 15:03:20 +0000

    fixed walmart repeat

--- public/javascripts/controller.js
+++ public/javascripts/controller.js
@@ -7,6 +7,9 @@ app.controller('MainCtrl', [
 
     $scope.getResults = function () {
          //var myCategory = document.getElementById("dropdown").value;
+
+        //$scope.walmartItems = [];
+         //$scope.ebayItems = [];
        
      var myQuery = document.getElementById("inputbox").value;
       var urls = "http://api.walmartlabs.com/v1/search?apiKey=2w9xg6wasagm2atc99qn8fjg&query="+myQuery+"&facet=on&facet.range=price:[4.50%20TO%205.01]&sort=bestseller";
@@ -19,20 +22,23 @@ app.controller('MainCtrl', [
         // console.log("Walmart");
         url: urls,
           dataType: 'jsonp',
+          async: false,
           success: function( data ){
             console.log("Hello from data");
             console.log(data);
             $("#result")
+            $scope.temp2 = [];
           $.each(data['items'], function(name2, value)
           {     
-
-		 $scope.walmartItems.push({
+      
+		 $scope.temp2.push({
 			name: value.name,
 			imgUrl: value.largeImage,
 			price: value.salePrice,
 			addToCartUrl: value.addToCartUrl
 
 		});
+     $scope.walmartItems = $scope.temp2;
    
   //           $("#result").append("<div class='items'><strong>"+value.name+"</strong><br><img src="+value.largeImage+"><br>Price: <strong>$" + value.salePrice+"</strong> <p></p><a href="+value.addToCartUrl+"><button type='button' class='btn btn-primary'>Click here to Buy</button></a></div><br><br>");
              //console.log(value.parentItemId);
@@ -45,7 +51,7 @@ app.controller('MainCtrl', [
             $.ajax({
     type: "GET",
     url: 'http://svcs.ebay.com/services/search/FindingService/v1?SECURITY-APPNAME=JacobWhi-CS201Fin-PRD-238c4f481-c3ec72bc&OPERATION-NAME=findItemsAdvanced&SERVICE-VERSION=1.0.0&RESPONSE-DATA-FORMAT=JSON&REST-PAYLOAD&keywords=' + myQuery + '&paginationInput.entriesPerPage=10&itemFilter(0).name=ListingType&itemFilter(0).value=FixedPrice&itemFilter(1).name=MinPrice&itemFilter(1).value=0&itemFilter(2).name=MaxPrice&itemFilter(2).value=5.00',
-    async: true, 
+    async: false, 
     dataType: 'jsonp',
     crossDomain:true,
     success: function(data, status, xhr){
